e762f9f988f648295909303b675f31bd2a5e89f7
Soundflog <108269837+Soundflog@users.noreply.github.com>
2025-03-13 14:03:35 +0300

    Update Main for test library

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import stringalgorithms.StringMatcher;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 class Main {
     public static void main(String[] args) {
+        StringMatcher matcher = new StringMatcher();
 
+        // Пример Рабина-Карпа
+        String text = "abracadabra";
+        String pattern = "cad";
+        List<Integer> index = matcher.rabinKarp(text, pattern);
+        System.out.println("Rabin-Karp result: " + index);
+
+        // Пример Z-функции
+        int[] z = matcher.zFunction("abracadabra");
+        System.out.println("Z-function result: " + Arrays.toString(z));
+
+        // Пример Кнута-Морриса-Пратта
+        List<Integer> indexKMP = matcher.knuthMorrisPratt(text, pattern);
+        System.out.println("Knuth-Morris-Pratt result: " + indexKMP);
     }
 }
